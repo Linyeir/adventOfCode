0bd46b9a79594dff4b8da41fa4f64031bc9bee0f
Linyeir <mail@linyeir.net>
2020-12-03 14:38:42 +0100

    3rd solution

---
+++ 03/script.js
@@ -0,0 +1,41 @@
+/*
+var input = ["..##.......",
+              "#...#...#..",
+              ".#....#..#.",
+              "..#.#...#.#",
+              ".#...##..#.",
+              "..#.##.....",
+              ".#.#.#....#",
+              ".#........#",
+              "#.##...#...",
+              "#...##....#",
+              ".#..#...#.#"]
+*/
+
+function execute(){
+    document.getElementById("output").innerHTML = checkDiagonal(3,1);
+}
+
+function executeBonus(){
+    console.log(checkDiagonal(1,1));
+    console.log(checkDiagonal(3,1));
+    console.log(checkDiagonal(5,1));
+    console.log(checkDiagonal(7,1));
+    console.log(checkDiagonal(1,2));
+
+    var product = checkDiagonal(1,1) * checkDiagonal(3,1)* checkDiagonal(5,1)* checkDiagonal(7,1)* checkDiagonal(1,2);
+
+    document.getElementById("outputBonus").innerHTML = product;
+}
+
+function checkDiagonal(right, down){
+    var count = 0;
+    for(var i = 0; i < input.length; i++){
+        if(i % down == 0){
+            if(input[i].charAt(((i/down)*right) % input[i].length) == "#"){
+                count++;
+            }
+        }
+    }
+    return count;
+}
