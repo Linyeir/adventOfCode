d6e12db561f7348afc589110e9b306710a3a6539
Linyeir <mail@linyeir.net>
2022-12-03 12:11:02 +0100

    day 03 2022

---
+++ 2022/Day-03-Rucksack-Reorganization/rucksackReorganization.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"unicode"
+)
+
+func main() {
+	//Read input file and add an empty line to the bottom, so the last elf doesnt get ignored
+	input, err := os.ReadFile("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	inputTmp := strings.TrimSuffix(string(input), "\n")
+	rucksacks := strings.Split(string(inputTmp), "\n")
+
+	// partOne(rucksacks)
+	partTwo(rucksacks)
+}
+
+func partOne(rucksacks []string) {
+	prioritySum := 0
+
+	for _, items := range rucksacks {
+		front, back := items[:len(items)/2], items[len(items)/2:]
+		for _, char := range front {
+			if strings.ContainsRune(back, char) {
+				prioritySum += priority(char)
+			}
+		}
+
+	}
+
+	fmt.Println("The sum of all priorities found in front and back pockets are: ", prioritySum)
+}
+
+func partTwo(rucksacks []string) {
+	prioritySum := 0
+
+	for i := 0; i < len(rucksacks); i += 3 {
+		for _, char := range rucksacks[i] {
+			if strings.ContainsRune(rucksacks[i+1], char) && strings.ContainsRune(rucksacks[i+2], char) {
+				prioritySum += priority(char)
+				break
+			}
+		}
+
+	}
+
+	fmt.Println("The sum of all priorities for each triple are: ", prioritySum)
+}
+
+func priority(item rune) int {
+	if unicode.IsLower(item) {
+		return int(item - 'a' + 1)
+	} else {
+		return int(item - 'A' + 27)
+	}
+}
